aef10d058d0ab780ac95f91689f304c907df60d8
Marco <Marco@147.32.218.45>
2013-03-12 16:03:45 +0100

    Byla  přidány funkce výběr slovníku s kterým chce uživatel pracovat  a možnost vytvářet nové slovníky (pozn. některé metody se musejí předělat spou s novým menu)

--- Slovnik/src/Semestralka1/Slovník.java
+++ Slovnik/src/Semestralka1/Slovník.java
@@ -8,6 +8,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,16 +19,16 @@ import java.util.logging.Logger;
  */
 public class Slovník {
 
-    Bunka prvni;
-    Bunka volna;
-    String typSlovniku;
+    private Bunka prvni;
+    private Bunka volna;
+   private  static String typSlovniku;
 
     public Slovník() {
     }
 
     public void vlozNaKonec(String aj, String cj, int spravneOdpovedi, int spatneOdpovedi) {  // metoda ktera vklada vstupni promene cj aj na konec spojoveho sezamu, pokud jsou promene praydne ulozeni se neprovede
         if (cj != null && aj != null) {
-            Slovo slovo = new Slovo(aj,cj,spravneOdpovedi,spatneOdpovedi);
+            Slovo slovo = new Slovo(aj, cj, spravneOdpovedi, spatneOdpovedi);
             Bunka bunka = new Bunka(slovo, null);
             if (prvni == null) {
                 prvni = bunka;
@@ -97,32 +98,75 @@ public class Slovník {
         }
         System.out.println("Dana dvojce byla smazana");
     }
-    public void vyberSlovnik(){
+
+    public void vyberSlovnik() {
         int volba;
-        Scanner scan=new Scanner(System.in);
-        while(true){
-        System.out.println("Prosim vzberte slovnik s kterym chcete pracovat");
-        System.out.println("1.Zakladni slovnik");
+        ArrayList seznam = new ArrayList();
+        Scanner scan = new Scanner(System.in);
+        seznam = nactiSeznam();
+        while (true) {
+            System.out.println("Prosim vzberte slovnik s kterym chcete pracovat");
+            for (int i = 0; i < seznam.size(); i++) {
+                System.out.println((i + 1) + ". " + seznam.get(i));
+            }
+            System.out.println("Nebo zadejte 0. Pokud chcete vztvo5it nový slovník");
             try {
-                    volba = scan.nextInt();
-                    if (volba > 0 && volba < 2) {
+                volba = scan.nextInt();
+                if (volba >= 0 && volba < seznam.size() + 1) {
+                    break;
+                } else {
+                    System.out.println("Chyba! Zadano spatne cislo. Opakujte prosim zadani.");
+                }
+            } catch (Exception f) {
+                System.out.println("Chyba! Zadan spatny znak. Opakujte prosim zadani.");
+                scan.next();
+            }
+        }
+        if (volba == 0) {
+            String nazev;
+            boolean test;
+            while (true) {
+                try {
+                    System.out.println("Prosim zadejte jmeno slovniku.");
+                    nazev = scan.next();
+                    test = seznam.contains(nazev);
+                    if (test == false) {
                         break;
                     } else {
-                        System.out.println("Chyba! Zadano spatne cislo. Opakujte prosim zadani.");
+                        System.out.println("Slovnik s timto nayvem jiy existuje. Prosim yvolte jiny nazev.");
                     }
-                } catch (Exception f) {
-                    System.out.println("Chyba! Zadan spatny znak. Opakujte prosim zadani.");
+                } catch (Exception e) {
+                    System.out.println("Byl yadan nepovoleny znak. Prosim opakujte yadani.");
                     scan.next();
                 }
-        }
-        if(volba==1){
-            typSlovniku="Zakladni";
+            }
+            vytvorSlovnik(nazev);
+        } else {
+            volba--;
+             setTypSlovniku((String) seznam.get(volba));
         }
     }
- /** Metoda vrátí jednu polozku z bunky. Proměnnou cisloBunky zvolíme ze které bunky spojevého seznamu chceme vytáhnout data.
-  * Proměnnou polozka volíme kterou plozku chceme z bunky vytáhnouta to tak že 1-vrátí cj výraz 2-vrátí anglický výraz 3- vrátí pocet spatnych odpovedi
-  * 4- vrátí pocet spravnych odpovedi.*/
-    public String getObsahBunky(int cisloBunky, int polozka) { 
+
+    public void vytvorSlovnik(String nazev) {
+        FileWriter out = null;
+        try {
+            out = new FileWriter(nazev + ".txt");
+            out.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Slovník.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        setTypSlovniku(nazev);
+        ulozSeznam(nazev);
+    }
+
+    /**
+     * Metoda vrátí jednu polozku z bunky. Proměnnou cisloBunky zvolíme ze které
+     * bunky spojevého seznamu chceme vytáhnout data. Proměnnou polozka volíme
+     * kterou plozku chceme z bunky vytáhnouta to tak že 1-vrátí cj výraz
+     * 2-vrátí anglický výraz 3- vrátí pocet spatnych odpovedi 4- vrátí pocet
+     * spravnych odpovedi.
+     */
+    public String getObsahBunky(int cisloBunky, int polozka) {
         int citac = 0;
         String slovo = null;
         Bunka pom = prvni;
@@ -132,64 +176,116 @@ public class Slovník {
                 if (polozka == 1) {
                     slovo = pom.slovicka.getCj();
                     break;
-                } else if(polozka==2){
+                } else if (polozka == 2) {
                     slovo = pom.slovicka.getAj();
                     break;
-                }else if(polozka==3){
-                   slovo =String.valueOf(pom.slovicka.getPocetSpatnychOdpovedi());
-                }else{
-                    slovo=String.valueOf(pom.slovicka.getPocetSpravnychOdpovedi());
+                } else if (polozka == 3) {
+                    slovo = String.valueOf(pom.slovicka.getPocetSpatnychOdpovedi());
+                } else {
+                    slovo = String.valueOf(pom.slovicka.getPocetSpravnychOdpovedi());
                 }
             }
-            pom=pom.dalsi;
+            pom = pom.dalsi;
         }
         return slovo;
 
     }
-    
-    public Slovník nactiSlovnik() { // Metoda ktera nacte slova ze souboru
+
+    public String getTypSlovniku() {
+        return typSlovniku;
+    }
+
+    public void setTypSlovniku(String typSlovniku) {
+        this.typSlovniku = typSlovniku;
+    }
+
+    public void  ulozSeznam( String novySlovnik){
+        ArrayList seznam = new ArrayList();
+        seznam=nactiSeznam();
+        seznam.add(novySlovnik);
+        FileWriter out = null;
+        try {
+            out = new FileWriter("Seznam.txt");
+        } catch (IOException ex) {
+            Logger.getLogger(Slovník.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try{
+        for (int i = 0; i < seznam.size(); i++) {
+            out.write(seznam.get(i) +""+System.lineSeparator());
+        }
+        out.close();
+        }catch(Exception e){
+            System.out.println("Chyba: Nebylo mozne provest yapis do souboru Seznam.txt");
+        }
+        
+    }
+    public ArrayList nactiSeznam() {
+        ArrayList seznam = new ArrayList();
+        Scanner scan = null;
+        String nazev;
+        boolean test;
+        try {
+            scan = new Scanner(new FileReader("Seznam.txt"));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Slovník.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        while (true) {
+            test = scan.hasNext();
+            if (test == true) {
+                nazev = scan.next();
+                seznam.add(nazev);
+            } else {
+                break;
+            }
+
+        }
+        return seznam;
+
+    }
+
+    public Slovník nactiSlovnik() {
         Slovník slovnik = new Slovník();
         int spravneOdpovedi, spatneOdpovedi;
-       boolean a;
+        boolean test;
         String cj, aj;
         Scanner scan = null;
         vyberSlovnik();
         try {
-            scan = new Scanner(new FileReader(typSlovniku+".txt"));
+            scan = new Scanner(new FileReader(getTypSlovniku() + ".txt"));
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Slovník.class.getName()).log(Level.SEVERE, null, ex);
         }
-         while(true){
-             a=scan.hasNext();
-             if(a == true){
-                 try{
-                 cj=scan.next();
-                 aj=scan.next();
-                 spravneOdpovedi=scan.nextInt();
-                 spatneOdpovedi=scan.nextInt();
-                   slovnik.vlozNaKonec(aj, cj, spravneOdpovedi, spatneOdpovedi);
-                 }catch(Exception e){
-                     System.out.println("Ze souboru nebylo mozne nacit data.Soubor je pravdepodobne pozkozen.");
-                 }
-             }else{
-                 break;
-             }
-         }
-        
-            
-       return slovnik;
+        while (true) {
+            test = scan.hasNext();
+            if (test == true) {
+                try {
+                    cj = scan.next();
+                    aj = scan.next();
+                    spravneOdpovedi = scan.nextInt();
+                    spatneOdpovedi = scan.nextInt();
+                    slovnik.vlozNaKonec(aj, cj, spravneOdpovedi, spatneOdpovedi);
+                } catch (Exception e) {
+                    System.out.println("Ze souboru nebylo mozne nacit data.Soubor je pravdepodobne pozkozen.");
+                }
+            } else {
+                break;
+            }
+        }
+
+
+        return slovnik;
     }
 
     public void ulozSlovnik() {  // Meotda ktera ulozi typSlovniku do souboru 
         FileWriter out = null;
         try {
-            out = new FileWriter(typSlovniku+".txt");
+            out = new FileWriter(getTypSlovniku()+ ".txt");
         } catch (IOException ex) {
             Logger.getLogger(Slovník.class.getName()).log(Level.SEVERE, null, ex);
         }
         for (int i = 1; i <= pocetDvojic(); i++) {
             try {
-                out.write(getObsahBunky(i, 1) + " " + getObsahBunky(i, 2) +" "+getObsahBunky(i,3)+" "+getObsahBunky(i,4)+System.lineSeparator());
+                out.write(getObsahBunky(i, 1) + " " + getObsahBunky(i, 2) + " " + getObsahBunky(i, 3) + " " + getObsahBunky(i, 4) + System.lineSeparator());
             } catch (IOException ex) {
                 Logger.getLogger(Slovník.class.getName()).log(Level.SEVERE, null, ex);
             }
