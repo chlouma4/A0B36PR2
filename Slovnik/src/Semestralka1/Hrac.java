728956b92d00a7deb8c293694dc3c4fcc88e9120
Marco <Marco@147.32.218.45>
2013-03-24 14:53:27 +0100

    Implementována metoda nastavHeslo - popis v javadoc.

--- Slovnik/src/Semestralka1/Hrac.java
+++ Slovnik/src/Semestralka1/Hrac.java
@@ -16,26 +16,30 @@ import java.util.logging.Logger;
  * @author Marco
  */
 public class Hrac {
-    private String nick;
-   
-    
-    public void  vytvorHrace(String nick,String heslo){
-       throw new UnsupportedOperationException("Not supported yet.");  
+
+    private String jmeno;
+    private String heslo;
+    private int pokusy;
+
+    public void vytvorHrace(String nick, String heslo) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    public void smazHrace(){
-        throw new UnsupportedOperationException("Not supported yet.");  
+
+    public void smazHrace() {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
+
     /**
      * Metoda nactiHrace vráti seznam hráčů i s jejich hesly v ArrayListu.
-     * 
-     * @return 
+     *
+     * @return
      */
-    public ArrayList nactiHrace(){
+    public ArrayList nactiHrace() {
         ArrayList seznam = new ArrayList();
         String hrac;
         boolean test;
         Scanner scan = null;
-         try {
+        try {
             scan = new Scanner(new FileReader("Hraci.txt"));
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Slovník.class.getName()).log(Level.SEVERE, null, ex);
@@ -52,31 +56,78 @@ public class Hrac {
         }
         return seznam;
     }
-    public void vyberHrace(){
+
+    public void vyberHrace() {
+        Scanner scan = new Scanner(System.in);
+        int volba;
         ArrayList hraci = new ArrayList();
-        hraci=this.nactiHrace();
+        hraci = this.nactiHrace();
         System.out.println("Vyberte prosim svuj ucet");
         for (int i = 0; i < hraci.size(); i++) {
-        System.out.println((i + 1) + ". " + hraci.get(i));
+            System.out.println((i + 1) + ". " + hraci.get(i));
         }
         System.out.println("Pokud chete vztvo5it novy ucet zvolte 0.");
+        volba = scan.nextInt();
+        if (volba == 0) {
+            this.jmeno = scan.next();
+            nastavHeslo(0);
+        }
     }
-    public void nastavHeslo(){
-        throw new UnsupportedOperationException("Not supported yet.");  
+
+    /**
+     * Metoda nastavHeslo slouzi k nastaveni hesla. Promenna prepinac slouzi pro
+     * rozliseni kdo chce nastavit nebo zmenit heslo. Pokud bude prepinac=0 tak
+     * se nastavy heslo pro noveho uzivatele. Pokud bude preponac=1 tak se
+     * nastavy nove heslo stavajiciho uzivatele pote co spravne zada sve doposud
+     * platne heslo.
+     *
+     * @param prepinac
+     */
+    public void nastavHeslo(int prepinac) {
+        Scanner scan = new Scanner(System.in);
+        boolean a;
+        String nove;
+        String nove1;
+        String stare;
+        if (prepinac == 1) {
+            while (true) {
+                System.out.println("Zadejte sve heslo");
+                stare = scan.next();
+                a = this.overHeslo(stare);
+                if (a == false) {
+                    this.pokusy();
+                    System.out.println("Heslo neni spravne. Pocet zbyvajicich pokusu"+(3-this.pokusy));
+                }else{
+                    break;
+                }
+            }
+        }
+        while(true){
+        System.out.println("Zadejte nove heslo");
+        nove=scan.next();
+        System.out.println("Zadejte nove heslo znovu");
+        nove1=scan.next();
+        if(nove.equals(nove1)){
+            this.heslo=nove;
+            break;
+        }else{
+            System.out.println("Zadana hesla se neshoduji. Prosim opakujte zadani");
+        }
+        }
     }
-    public void overHeslo(){
-        throw new UnsupportedOperationException("Not supported yet.");   
+
+    public boolean overHeslo(String heslo) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    public void zmenHeslo(){
-        throw new UnsupportedOperationException("Not supported yet.");   
+    public void pokusy(){
+         throw new UnsupportedOperationException("Not supported yet.");
     }
-    public void zasifrujHeslo(){
-         throw new UnsupportedOperationException("Not supported yet.");  
+
+    public void zasifrujHeslo() {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    public void rozsifrujHeslo(){
-        throw new UnsupportedOperationException("Not supported yet."); 
+
+    public void rozsifrujHeslo() {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
-    
-    
 }
