f587433ac4d95e297032d7c4d7693188ef94b8de
Marco <Marco@147.32.218.45>
2013-03-24 13:15:24 +0100

    Ve třídě Hrac bylo vytvořeno nových 9 metod které budou postupně implementovány

---
+++ Slovnik/src/Semestralka1/Hrac.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Semestralka1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Marco
+ */
+public class Hrac {
+    private String nick;
+   
+    
+    public void  vytvorHrace(String nick,String heslo){
+       throw new UnsupportedOperationException("Not supported yet.");  
+    }
+    public void smazHrace(){
+        throw new UnsupportedOperationException("Not supported yet.");  
+    }
+    public ArrayList nactiHrace(){
+        throw new UnsupportedOperationException("Not supported yet.");  
+    }
+    public void vyberHrace(){
+        ArrayList hraci = new ArrayList();
+        hraci=this.nactiHrace();
+    }
+    public void nastavHeslo(){
+        throw new UnsupportedOperationException("Not supported yet.");  
+    }
+    public void overHeslo(){
+        throw new UnsupportedOperationException("Not supported yet.");   
+    }
+    public void zmenHeslo(){
+        throw new UnsupportedOperationException("Not supported yet.");   
+    }
+    public void zasifrujHeslo(){
+         throw new UnsupportedOperationException("Not supported yet.");  
+    }
+    public void rozsifrujHeslo(){
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
+    
+    
+}
